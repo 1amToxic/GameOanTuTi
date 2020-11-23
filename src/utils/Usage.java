a2257fa92d40b938a9a1208555c678c2d8c7ebd1
Xuân Kiên <kienlovend@gmail.com>
2020-11-23 16:10:19 +0700

    delete server from client

--- src/utils/Usage.java
+++ src/utils/Usage.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,7 +7,7 @@ package utils;
 
 /**
  *
- * @author lamit
+ * @author Admin
  */
 public interface Usage {
     public int port = 4567;
